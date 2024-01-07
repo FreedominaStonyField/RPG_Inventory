7cc5814fa1e13412133828b337668d85edf82340
FreedominaStonyField <stanfield.francisco2021@gmail.com>
2024-01-07 13:51:08 -0500

    created get item function in database

--- ItemDatabase/game_item_class.gd
+++ ItemDatabase/game_item_class.gd
@@ -4,5 +4,5 @@ class_name game_item
 
 @export_category("Item Info")
 
-@export var name : String = "Placeholder"
+@export var item_name : String = "Placeholder"
 @export_enum("Item", "Healing", "Equipment", "Indispensable") var item_catagory: String
--- ItemDatabase/item_database.gd
+++ ItemDatabase/item_database.gd
@@ -1,1 +1,15 @@
 extends Node
+
+
+var items : Array
+
+func load_items():
+	pass
+
+func get_item(item_name):
+	
+	for i in items:
+		if i.item_name == item_name:
+			return i
+		else:
+			print(item_name + " not found in databese")
