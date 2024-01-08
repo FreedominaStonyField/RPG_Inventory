72e151903f666cec192e4c214f5bfab714548b12
Francisco Stanfield <108752839+FreedominaStonyField@users.noreply.github.com>
2024-01-08 01:50:35 -0500

    added simple UI update system

--- UI.gd
+++ UI.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func _ready():
-	
+	player_inventory.connect('inventory_changed', update_ui)
 	hide()
 
 func _input(event):
@@ -13,3 +13,13 @@ func _input(event):
 		player_inventory.add_item("Vibe Wand", 1)
 		get_tree().paused = !get_tree().paused
 		visible = !visible
+		
+func update_ui(inventory):
+	
+	for i in $ScrollContainer/ItemList.get_children():
+		i.queue_free()
+	for i in inventory._items:
+		var item_label = Label.new()
+		item_label.text = "{} x{}".format([i.item_reference.item_name, i.quantity], "{}")
+		$ScrollContainer/ItemList.add_child(item_label)
+
