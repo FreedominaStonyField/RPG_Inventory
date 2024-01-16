9669291c957f4a559784a9df23e263cfc9b8f120
FreedominaStonyField <stanfield.francisco2021@gmail.com>
2024-01-16 10:37:57 -0500

    Basic Interaction Area for player

--- player_spawner/PlayerSpawner.gd
+++ player_spawner/PlayerSpawner.gd
@@ -3,5 +3,6 @@ extends Marker3D
 @export var player_scene : PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#Dialogic.start("welcome_game_start")
 	add_child(player_scene.instantiate())
 	#hide()
